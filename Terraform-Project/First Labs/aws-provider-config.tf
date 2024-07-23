af1b6dfdd0b5b4904aea79e5d42b52d02c610830
Mian Rashid <ahaddrashid@gmail.com>
2024-07-23 17:36:30 +0200

    Changes

--- Terraform-Project/First Labs/aws-provider-config.tf
+++ Terraform-Project/First Labs/aws-provider-config.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.58.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
 provider "aws" {
   region     = "eu-north-1"
 }
