6cd52c0767725f6ef97cb363e8ae9ce0151942a7
Mian Rashid <ahaddrashid@gmail.com>
2024-07-27 04:33:24 +0200

    Updated formating on data_sources file.
    Added a dynamic_ block project.

--- Terraform-Project/Read, Generate, Modify Configurations/Data_sources/Data_source.tf
+++ Terraform-Project/Read, Generate, Modify Configurations/Data_sources/Data_source.tf
@@ -3,16 +3,16 @@ locals {
 }
 
 data "aws_ami" "myimage" {
-    most_recent = true
-    owners = ["amazon"]
+  most_recent = true
+  owners      = ["amazon"]
 
-    filter {
-      name = "name"
-      values = ["Ubuntu_20.04-x86_64-SQL_2022_Standard-*"]
-    }
+  filter {
+    name   = "name"
+    values = ["Ubuntu_20.04-x86_64-SQL_2022_Standard-*"]
+  }
 }
 
 resource "aws_instance" "web" {
-    ami = data.aws_ami.myimage.image_id
-    instance_type = local.small_image
+  ami           = data.aws_ami.myimage.image_id
+  instance_type = local.small_image
 }
