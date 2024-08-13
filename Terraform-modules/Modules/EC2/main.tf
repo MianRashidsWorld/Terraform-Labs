7ea49727dd6d9bd0583bb3d33753de0f47915e79
Mian Rashid <ahaddrashid@gmail.com>
2024-08-13 17:24:54 +0200

    improved modules

--- Terraform-modules/Modules/EC2/main.tf
+++ Terraform-modules/Modules/EC2/main.tf
@@ -2,17 +2,32 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.6.1"
+      version = ">= 5.6.1"
     }
   }
 }
 
+provider "aws" {
+  region = var.region
+}
+
 resource "aws_instance" "myec2" {
-  ami = "ami-0249211c9916306f8"
-  instance_type = "t3.micro"
+  ami = var.ami
+  instance_type = var.instance_type
 
   tags = {
     Name = "My-first-ec2"
   }
 }
 
+variable "region" {
+  
+}
+
+variable "ami" {
+  
+}
+
+variable "instance_type" {
+  
+}
--- Terraform-modules/Teams/A/module.tf
+++ Terraform-modules/Teams/A/module.tf
@@ -1,3 +1,6 @@
 module "ec2" {
   source = "../../Modules/EC2"
+  instance_type = "t3.micro"
+  ami = "ami-123"
+  region = "eu-north-1"
 }
